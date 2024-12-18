1d52b52f61df51a5c1bb27f443138c59c20dd910
pablosingh <pablo22_4@hotmail.com>
2024-12-17 23:11:55 -0300

    Cripto Routes

---
+++ api/src/routes/cripto.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import {
+    addCripto,
+    getCriptos,
+} from "../controllers/criptos/cripto.controllers.js";
+import { getActualPriceDB } from "../controllers/criptos/getActualPrice.controllers.js";
+const router = Router();
+
+router.get("/criptos", getCriptos); // Trae todas las criptos de la DB
+router.get("/dayprice/:cripto", getActualPriceDB); // cripto mas actual
+router.post("/cripto", addCripto);
+
+export default router;
