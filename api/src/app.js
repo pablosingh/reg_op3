ba144ec49f8f0509610cb4fe54733ec5add87052
pablosingh <pablo22_4@hotmail.com>
2024-12-19 00:03:31 -0300

    configuracion de middlewares en app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,31 @@
 import express from "express";
+import criptoRoutes from "./routes/cripto.routes.js";
+import holdingRoutes from "./routes/holdings.routes.js";
+import operationsRoutes from "./routes/operations.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+import usersRoutes from "./routes/users.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE",
+    );
+    next();
 });
+
+app.use(criptoRoutes);
+app.use(holdingRoutes);
+app.use(operationsRoutes);
+app.use(tasksRoutes);
+app.use(usersRoutes);
 
 export default app;
